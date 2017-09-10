9d247d21e71dacf455e5f2936883364ff4c81b91
grobikon <grobikon@mail.ru>
2017-09-10 23:12:39 +0300

    FieldTest edit

--- test/ru/grobikon/xo/model/FieldTest.java
+++ test/ru/grobikon/xo/model/FieldTest.java
@@ -1,6 +1,7 @@
 package ru.grobikon.xo.model;
 
 import org.junit.Test;
+import ru.grobikon.xo.model.exceptions.AlreadyOccupiedException;
 import ru.grobikon.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
@@ -26,6 +27,21 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure, actualFigure);
+    }
+
+    @Test
+    public void setFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e){
+
+        }
     }
 
     @Test
