ee57c363613d60df800237ab0c36f259ca1d0994
grobikon <grobikon@mail.ru>
2017-09-11 23:41:50 +0300

    Field Test  with Field (with flexible fieldsize)

--- test/ru/grobikon/xo/model/FieldTest.java
+++ test/ru/grobikon/xo/model/FieldTest.java
@@ -12,14 +12,14 @@ public class FieldTest {
 
     @Test
     public void getSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(3, field.getSize());
     }
 
     @Test
     public void setFigure() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
 
@@ -31,7 +31,7 @@ public class FieldTest {
 
     @Test
     public void getFigureWhenFigureIsNotSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -41,7 +41,7 @@ public class FieldTest {
 
     @Test
     public void getFigureWhenXIsLessThenZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1, 0);
 
         try {
@@ -54,7 +54,7 @@ public class FieldTest {
 
     @Test
     public void getFigureWhenYIsLessThenZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, -1);
 
         try {
@@ -67,7 +67,7 @@ public class FieldTest {
 
     @Test
     public void getFigureWhenYIsMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize() + 1, 0);
 
         try {
@@ -80,7 +80,7 @@ public class FieldTest {
 
     @Test
     public void getFigureWhenXIsMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, field.getSize() + 1);
 
         try {
